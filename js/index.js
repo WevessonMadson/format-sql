a3f632b3d3dbf342cba1d7f4c9fb82742caff203
WevessonMadson <wevesson.madson@hotmail.com>
2023-01-31 23:20:34 -0300

    adicionada a opção de pesquisa de campo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-function getKeys(sql) {
+function getFields(sql) {
     try {
         const initialIndex = sql.indexOf("(") + 1;
         const finalIndex = sql.indexOf(")");
 
-        const keys = sql.substring(initialIndex, finalIndex).split(",").map(element => element.trim());
+        const fields = sql.substring(initialIndex, finalIndex).split(",").map(element => element.trim());
 
-        return keys;
+        return fields;
 
     } catch (error) {
         console.log(error);
@@ -16,11 +16,11 @@ function getKeys(sql) {
 
 function getValues(sql) {
     try {
-        const strSqlNotKeys = sql.slice(sql.indexOf("(") + 1);
-        const initialIndex = strSqlNotKeys.indexOf("(") + 1;
-        const finalIndex = strSqlNotKeys.lastIndexOf(")");
+        const strSqlNotfields = sql.slice(sql.indexOf("(") + 1);
+        const initialIndex = strSqlNotfields.indexOf("(") + 1;
+        const finalIndex = strSqlNotfields.lastIndexOf(")");
 
-        const values = strSqlNotKeys.substring(initialIndex, finalIndex).split(",").map(element => element.trim().replace(/[",',]/g, ""));
+        const values = strSqlNotfields.substring(initialIndex, finalIndex).split(",").map(element => element.trim().replace(/[",',]/g, ""));
         return values;
 
     } catch (error) {
@@ -30,12 +30,12 @@ function getValues(sql) {
     }
 }
 
-function getObjectSql(keys, values) {
+function getObjectSql(fields, values) {
     try {
         let objectData = {};
 
-        for (let i = 0; i < keys.length; i++) {
-            objectData[keys[i]] = values[i];
+        for (let i = 0; i < fields.length; i++) {
+            objectData[fields[i]] = values[i];
         }
 
         return objectData;
@@ -54,13 +54,13 @@ function getFormatSql(sql) {
         if (sqlFormated.indexOf("insert ") == -1 && sqlFormated.indexOf(" into") == -1) {
             return "O sql passado não se trata de um insert ou está escrito incorretamente, verifique.";
         } else {
-            let keys = getKeys(sql);
+            let fields = getFields(sql);
             let values = getValues(sql);
 
-            if (keys.length != values.length) {
-                return `A quantidade de chaves (campos) é diferente da quantidade de valores, verifique.`;
+            if (fields.length != values.length) {
+                return `A quantidade de campos é diferente da quantidade de valores, verifique.`;
             } else {
-                return getObjectSql(keys, values);
+                return getObjectSql(fields, values);
             }
         }
 
@@ -69,6 +69,26 @@ function getFormatSql(sql) {
 
         return "Ocorreu um erro inesperado, verifique no console.";
     }
+}
+
+function renderTable(dataTables) {
+    return `
+    <div id='busca'>
+        <input type='text' placeholder='filtrar campo' id='pesquisar'>
+        <button id='buscar' onclick='searchFields()' title='filtrar'><span class="material-icons">search</span></button>
+        <button id='limpar' onclick='clearFields()' title='limpar filtro'><span class="material-icons">clear</span></button>
+    </div>    
+    <table id='customers'>
+        <thead>
+            <tr>
+                <th>CAMPO</th>
+                <th>VALOR</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${dataTables}
+        </tbody>
+    </table>`;
 }
 
 function renderReturn(e) {
@@ -91,23 +111,15 @@ function renderReturn(e) {
 
         return;
     } else {
+        localStorage.setItem("objectTable", JSON.stringify(objectFormated));
+
         let dataTable = "";
+
         for (key in objectFormated) {
             dataTable += `<tr><td>${key}</td><td>${objectFormated[key]}</td></tr>`;
         }
 
-        result.innerHTML = `
-    <table id='customers'>
-        <thead>
-            <tr>
-                <th>CAMPO</th>
-                <th>VALOR</th>
-            </tr>
-        </thead>
-        <tbody>
-            ${dataTable}
-        </tbody>
-    </table>`;
+        result.innerHTML = renderTable(dataTable);
     }
 }
 
@@ -116,7 +128,7 @@ function darkMode(e) {
 
     const button_dark = document.querySelector("#dark-mode");
     const html = document.querySelector("html");
-    
+
     html.classList.toggle("dark-mode");
 
     if (button_dark.innerText == "light_mode") {
@@ -141,3 +153,38 @@ function reloadDarkMode() {
 document.getElementById("gerar").addEventListener("click", renderReturn);
 document.getElementById("dark-mode").addEventListener("click", darkMode);
 window.addEventListener("DOMContentLoaded", reloadDarkMode);
+
+function searchFields() {
+    const objectFormated = JSON.parse(localStorage.getItem("objectTable"));
+
+    const search = document.getElementById("pesquisar").value.toLocaleLowerCase();
+
+    let dataTable = "";
+
+    if (search != null) {
+        for (key in objectFormated) {
+            let field = key.toLocaleLowerCase().indexOf(search);
+            if (field != -1) {
+                dataTable += `<tr><td>${key}</td><td>${objectFormated[key]}</td></tr>`;
+            }
+        }
+        
+        if (dataTable == "") {
+            alert("Nenhum campo foi encontrado com esse parâmetro.")
+        } else {
+            result.innerHTML = renderTable(dataTable);
+        }
+    }
+}
+
+function clearFields() {
+    const objectFormated = JSON.parse(localStorage.getItem("objectTable"));
+
+    dataTable = "";
+
+    for (key in objectFormated) {
+        dataTable += `<tr><td>${key}</td><td>${objectFormated[key]}</td></tr>`;
+    }
+
+    result.innerHTML = renderTable(dataTable);
+}
